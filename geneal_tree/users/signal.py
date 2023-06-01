958f40bd57d9df0c5b353a672768637f32901b65
Danil Mazurkin <mazurkin.daniel@gmail.com>
2023-06-02 02:16:30 +1000

    Отправка сообщение в rabbitmq

--- geneal_tree/users/signal.py
+++ geneal_tree/users/signal.py
@@ -1,9 +1,18 @@
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from users.models import AllowedUser
+from django.conf import settings
+import pika
 
 
 @receiver(pre_save, sender=AllowedUser)
 def allowed_user_signal(sender, instance, **kwargs):
     """Сигнал используется для проверки есть ли такой зарегистрированный пользователь."""
-    print("Сигнал сработал")
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host=settings.BROKER_URL))
+    channel = connection.channel()
+    channel.queue_declare(queue='auth_queue')
+    channel.basic_publish(exchange='',
+                          routing_key='auth_queue',
+                          body='Hello World!')
+    connection.close()
