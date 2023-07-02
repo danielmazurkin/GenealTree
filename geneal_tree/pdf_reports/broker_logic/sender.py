500d8297c687bd49b06d4d69545d2cbb123435b3
Danil Mazurkin <mazurkin.daniel@gmail.com>
2023-07-02 19:50:48 +1000

    Поправил sender в rabbit

--- geneal_tree/pdf_reports/broker_logic/sender.py
+++ geneal_tree/pdf_reports/broker_logic/sender.py
@@ -8,7 +8,6 @@ class SenderToServicePDF(SenderToServiceBase):
     QUEUE_NAME = 'pdf_forming'
 
     def send_data(self, body):
-        print("self.priority = ", self.priority)
         self.channel.basic_publish(exchange='',
                                    routing_key=self.QUEUE_NAME,
                                    body=body)
